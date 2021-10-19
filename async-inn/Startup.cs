652ea6a029c3110b919a020ac4914dc6163c672f
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-19 15:56:05 -0500

    add Iconfig and DbContext()

--- async-inn/Startup.cs
+++ async-inn/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using async_inn.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,16 +16,23 @@ namespace async_inn
     public class Startup
     {
 
-    
+        public IConfiguration Configuration { get; }
 
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-          
+            services.AddDbContext<AsyncInnDbContext>(options => {
+                // Our DATABASE_URL from js days
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
         }
-    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
