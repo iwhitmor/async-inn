6d95857bdc53c3b4de1551bbadd187dd435e56a1
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-20 16:35:09 -0500

    take dependency on scoped DbContext for amenities

--- async-inn/Services/DatabaseAmenityRepository.cs
+++ async-inn/Services/DatabaseAmenityRepository.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using async_inn.Data;
+
 namespace async_inn.Services
 {
     public class DatabaseAmenityRepository : IAmenityRepository
     {
+        private readonly AsyncInnDbContext _context;
 
+        public DatabaseAmenityRepository(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
     }
 }
