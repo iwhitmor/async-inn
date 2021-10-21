9b50f1c9b2473f2bd4f58b323360a09c508127b8
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-20 22:48:44 -0500

    take dependency on our scoped RoomDbContext

--- async-inn/Services/DatabaseRoomRepository.cs
+++ async-inn/Services/DatabaseRoomRepository.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using async_inn.Data;
+
 namespace async_inn.Services
 {
     public class DatabaseRoomRepository : IRoomRepository
     {
-       
+        private readonly AsyncInnDbContext _context;
+
+        public DatabaseRoomRepository(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
     }
 }
