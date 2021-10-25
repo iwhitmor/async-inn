a021f5a80d22f41c52ffbcf7fbe3fc392852102a
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-24 22:31:54 -0500

    add new migration and update for roomAmenities

---
+++ async-inn/Migrations/20211025032827_AddRoomAmenities.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace async_inn.Migrations
+{
+    public partial class AddRoomAmenities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoomAmenities",
+                columns: table => new
+                {
+                    AmenityId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomAmenities", x => new { x.AmenityId, x.RoomId });
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Amenities_AmenityId",
+                        column: x => x.AmenityId,
+                        principalTable: "Amenities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomAmenities_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "RoomAmenities",
+                columns: new[] { "AmenityId", "RoomId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "RoomAmenities",
+                columns: new[] { "AmenityId", "RoomId" },
+                values: new object[] { 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "RoomAmenities",
+                columns: new[] { "AmenityId", "RoomId" },
+                values: new object[] { 3, 3 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomAmenities_RoomId",
+                table: "RoomAmenities",
+                column: "RoomId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomAmenities");
+        }
+    }
+}
