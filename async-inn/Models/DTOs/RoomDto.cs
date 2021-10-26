b85ae5551140e2ddeacb0d3b2395fabe2b257317
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-26 13:56:46 -0500

    create RoomDTO file

---
+++ async-inn/Models/DTOs/RoomDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace async_inn.Models.DTOs
+{
+    public class RoomDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Layout { get; set; }
+        public List<AmenityDTO> Amenities { get; set; }
+    }
+}
